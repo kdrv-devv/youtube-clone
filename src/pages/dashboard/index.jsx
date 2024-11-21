c14663e5eab8adcf1335398ecba73f7464873fc6
kdrv dev <ffeodalik@gmail.com>
2024-11-21 18:45:41 +0500

    Navigate qoshildi

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Link, Outlet, useLocation } from 'react-router-dom'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { MdHomeFilled } from "react-icons/md";
 import { FaCompass } from "react-icons/fa6";
 import { MdOutlineSubscriptions } from "react-icons/md";
 
 const Dashboard = () => {
   const {pathname} = useLocation()
-  console.log(pathname);
-  
+  const navigate  = useNavigate()  
   const DashBoardStyle = 'pl-[10px] w-full h-[40px]  flex items-center gap-3 cursor-pointer'
 
   return (
@@ -17,23 +16,21 @@ const Dashboard = () => {
         <div className= 'flex flex-col  pt-[10px]  w-[300px] h-[673px] bg-[#212121]'>
                <div className={`${DashBoardStyle} ${pathname === "/" && "bg-[#303030]"}`}>
                <MdHomeFilled  style={{ color: "white", fontSize: "30px" }}  />
-               <Link to={`/`}  ><h5 className=' text-white text-[19px]'>Home</h5> </Link>  
+               <button onClick={()=>{navigate("/")}}><h5 className=' text-white text-[19px]'>Home</h5></button>
                </div>
 
 
                <div className='pl-[10px] w-full h-[40px]  flex items-center gap-3 cursor-pointer'>
                <FaCompass  style={{ color: "white", fontSize: "25px" }}  />
-               <Link to={`/explore`}  ><h5 className=' text-white text-[19px]'>Explore</h5> </Link>  
+               <button onClick={()=>{navigate("/explore")}}><h5 className=' text-white text-[19px]'>Explore</h5></button>
                </div>
 
 
                <div className='pl-[10px] w-full h-[40px]  flex items-center gap-3 cursor-pointer'>
                <MdOutlineSubscriptions style={{ color: "white", fontSize: "25px" }}  />
-               <Link to={`/subscribe`}  ><h5 className=' text-white text-[19px]'>Subscriptions</h5> </Link>  
+               <button onClick={()=>{navigate("/subscribe")}}><h5 className=' text-white text-[19px]'>Subscriptions</h5></button>
                </div>
          
-         
-          
 
         </div>
         <Outlet/>
